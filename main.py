bbfaf6f8b78d11be31566952d1ce2b620e55ad82
vialjb <jvial8@myges.fr>
2025-01-27 22:48:18 +0100

    ajouts des fonctions  :      - def is_prime      - def random_nb Modification de la fonction key_gen :      - génération des nombres premier sous condition qu'ils ne soient pas égaux entre eux

--- main.py
+++ main.py
@@ -34,40 +34,38 @@ Bravo, vous avez une clé privée : n et d et une clé publique : n et e
 
 def is_prime(n):
     if n <= 1:
-        return False  # Les nombres <= 1 ne sont pas premiers
-    if n == 2:
-        return True  # 2 est un nombre premier
-    if n % 2 == 0:
-        return False  # Les nombres pairs autres que 2 ne sont pas premiers
-    for i in range(3, int(math.sqrt(n)) + 1, 2):  # On teste uniquement les impairs
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
-            return False  # Si n est divisible par i, il n'est pas premier
-    return True  # Si on n'a trouvé aucun diviseur, alors c'est un nombre premier
+            return False
+    return True
 
+def random_nb():
+    return random.randint(1000000000, 9999999999)
 
 def key_gen():
-    p = random.randint(1000000000, 9999999999)
-    q = random.randint(1000000000, 9999999999)
-    e = random.randint(1000000000, 9999999999)
-    d = random.randint(1000000000, 9999999999)
-    a = 0
-    b = 0
-    while a < 1 :
-        if is_prime(p):
-         print("Le premier nombre est premier !")
-         a = 1
-        else:
-         key_gen()
-    while b < 1 :
-        if is_prime(q):
-           print("Le deuxième nombre est premier !")
-        b = 1
-        else:
-          key_gen()
-    if p != q and e != d:
-        n = p * q
-        n2 = (p - 1) * (q - 1)
-        ed = e * d
+    p = random_nb()
+    q = random_nb()
+    e = random_nb()
+    d = random_nb()
+
+    while not is_prime(p) :
+       p = random_nb()
+
+    while not is_prime(q) and q != p:
+        q = random_nb()
+
+    while not is_prime(e) and e != q and e != p:
+        e = random_nb()
+
+    while not is_prime(d) and d != e and d != p and d != q:
+        d = random_nb()
+
+
+
+    n = p * q
+    n2 = (p - 1) * (q - 1)
+    ed = e * d
 
 
 
